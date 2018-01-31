4e2ac4a05298ae9c0d1c8b1b0ed35f29fff30703
Devansh Gaur <devansh.star@gmail.com>
2018-01-31 21:16:32 +0530

    Beginner done

---
+++ 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("positive\n");
+    }
+    else if(a<0)
+    {
+        printf("negative\n");
+    }
+    else{
+            printf("zero\n");
+        }
+}
